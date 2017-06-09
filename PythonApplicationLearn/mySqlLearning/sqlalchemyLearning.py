2ab1acdd6b6430e3ca594b4bb7e1ca3eebf61e5b
lovebanlan <lovebanlan@foxmail.com>
2017-06-09 11:08:15 +0800

    learning

--- PythonApplicationLearn/mySqlLearning/sqlalchemyLearning.py
+++ PythonApplicationLearn/mySqlLearning/sqlalchemyLearning.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, create_engine
+﻿from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,5 +7,14 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(String(20), primary_kay=True)
+    id = Column(String(20), primary_key=True)
     name = Column(String(20))
+
+engine = create_engine('mysql+pymysql://root:password@localhost:3306/test')
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+new_user = User(id='3', name='jim')
+session.add(new_user)
+session.commit()
+session.close()
